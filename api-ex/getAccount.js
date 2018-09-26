083c3108c91b97e1503da8f2a8b543cb29cf0c94
dongjun.kwon <mayajuni10@gmail.com>
2018-09-27 08:44:24 +0900

    getAccount

--- api-ex/getAccount.js
+++ api-ex/getAccount.js
@@ -5,12 +5,12 @@ const config = {
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
 };
 
-Eos(config).getAccount('zxcvbasdfg11', (error, account) => {
-    if(error) {
-        console.error(error);
-    }
-
-    console.log(account);
-});
+// Eos(config).getAccount('zxcvbasdfg11', (error, account) => {
+//     if(error) {
+//         console.error(error);
+//     }
+//
+//     console.log(account);
+// });
 
 Eos(config).getAccount({account_name: 'zxcvbasdfg11'}).then(console.log).catch(console.error);
--- wallet/js/eos.js
+++ wallet/js/eos.js
@@ -1,10 +1,41 @@
+const config = {
+    httpEndpoint: 'https://api-kylin.eosasia.one',
+    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
+};
+
 function getAccountInfo() {
     document.getElementById('info').style.display = 'none';
     document.getElementById('error').style.display = 'none';
 
     const accountName = document.getElementById('accountName').value;
-    if(accountName) {
-        document.getElementById('info').style.display = 'block';
+    if (accountName) {
+        Eos(config).getAccount(accountName).then(account => {
+            document.getElementById('account').innerHTML = account.account_name;
+
+            const stake = account.voter_info ? account.voter_info.staked / 10000 : 0;
+            const unstake = Number(account.core_liquid_balance.replace('EOS', ''));
+            const refund = account.refund_request ?
+                Number(account.refund_request.net_amount.replace('EOS', '')) + Number(account.refund_request.cpu_amount.replace('EOS', ''))
+                : 0;
+            const total = stake + unstake + refund;
+
+            document.getElementById('total').innerHTML = total + ' EOS';
+            document.getElementById('stake').innerHTML = stake + ' EOS';
+            document.getElementById('unstake').innerHTML = unstake + ' EOS';
+            document.getElementById('refund').innerHTML = refund + ' EOS';
+
+            document.getElementById('cpu').innerHTML =
+                account.cpu_limit.used + 'up / ' + account.cpu_limit.max + 'up (' + account.total_resources.cpu_weight + ')';
+            document.getElementById('net').innerHTML =
+                account.net_limit.used + 'bytes / ' + account.net_limit.max + 'bytes (' + account.total_resources.net_weight + ')';
+            document.getElementById('ram').innerHTML =
+                account.ram_quota + 'bytes / ' + account.ram_quota + 'bytes';
+
+            document.getElementById('info').style.display = 'block';
+        }).catch(e => {
+            document.getElementById('error').style.display = 'block';
+            document.getElementById('errorMessage').innerHTML = e.message;
+        });
     } else {
         document.getElementById('error').style.display = 'block';
         document.getElementById('errorMessage').innerHTML = 'EOS Account Name을 입력해주세요.';
